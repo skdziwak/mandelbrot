407e1b26f421cb8ce7b483190b93b3a117a57470
skdziwak <skdziwak@gmail.com>
2021-04-02 23:33:03 +0200

    Fixed args bug and added invert colors option

--- render.py
+++ render.py
@@ -13,10 +13,10 @@ def read(path):
         return f.read()
 
 parser = argparse.ArgumentParser(description='Mandelbrot Set zoom animation generator.')
-parser.add_argument('--bw', nargs='?', action='store', default=32, type=int, help='Block width')
-parser.add_argument('--bh', nargs='?', action='store', default=32, type=int, help='Block height')
-parser.add_argument('--gw', nargs='?', action='store', default=48, type=int, help='Grid width')
-parser.add_argument('--gh', nargs='?', action='store', default=48, type=int, help='Grid height')
+parser.add_argument('-bw', nargs='?', action='store', default=32, type=int, help='Block width')
+parser.add_argument('-bh', nargs='?', action='store', default=32, type=int, help='Block height')
+parser.add_argument('-gw', nargs='?', action='store', default=48, type=int, help='Grid width')
+parser.add_argument('-gh', nargs='?', action='store', default=48, type=int, help='Grid height')
 parser.add_argument('-l', nargs='?', action='store', default=500, type=int, help='Iterations limit')
 parser.add_argument('-x', nargs='?', action='store', default=0, type=float, help='X offset')
 parser.add_argument('-y', nargs='?', action='store', default=0, type=float, help='Y offset')
@@ -25,9 +25,11 @@ parser.add_argument('-z', nargs='?', action='store', default=1.02, type=float, h
 parser.add_argument('-cm', nargs='?', action='store', default='PuBuGn', type=str, help='Matplotlib colormap')
 parser.add_argument('frames', type=int, help='Number of rendered frames')
 parser.add_argument('output', type=str, help='Output file (mp4)')
+parser.add_argument('-i', action='store_true', help='Invert colormap')
 
 args = parser.parse_args()
 
+INVERT = args.i
 BLOCK = (args.bw, args.bh, 1)
 GRID = (args.gw, args.gh)
 FRAMES = args.frames
@@ -67,6 +69,8 @@ try:
 
         result = plt.get_cmap(CM)(dest)
         result *= 255
+        if INVERT:
+            result = 1 - result
 
         Image.fromarray(result.astype('uint8'), 'RGBA').save('tmp/frame{}.png'.format(f + 1))
 except KeyboardInterrupt:
